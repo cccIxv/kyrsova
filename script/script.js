9c6236c7e4dc74ffa6c0dc2a2e2609e09f48198f
NBG3 <nazarij.krech@gmail.com>
2024-11-16 18:14:30 +0200

    148

--- script/script.js
+++ script/script.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-//Модальне вікнo
+//Модальне вікно
 document.addEventListener('DOMContentLoaded', () => {
     const modal = document.getElementById('modal');
     const contactLink = document.getElementById('contact-link');
